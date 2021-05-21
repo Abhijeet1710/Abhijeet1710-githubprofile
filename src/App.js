76f2aa0204346442af7b8a305588a95ba3858f7d
Abhijeet Khamkar <52439127+Abhijeet1710@users.noreply.github.com>
2021-05-21 19:38:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
             <input type="text" aria-label="First name" class="form-control" id="input"/>
         </div>
 
-        <button className="btn btn-search mt-2" onClick={ () => { getInfo() } }> Search</button>
+        <button className="btn btn-search mt-2" onClick={ () => { getInfo() } }> Search User</button>
 
      </div>
 
